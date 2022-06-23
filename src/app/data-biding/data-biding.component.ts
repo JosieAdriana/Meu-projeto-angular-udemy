f19fe6a9cdd21c4afde7034b7824d07695b648b6
Josie Flôres <drijosie@gmail.com>
2022-06-23 11:30:11 -0300

    Property Binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -11,6 +11,9 @@ export class DataBidingComponent implements OnInit {
   public idade: number = 42;
   public maisUm: number = 1;
 
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular-400x250.png";
+  public imgTitle: string = "Property Binding";
   constructor() { }
 
   ngOnInit(): void {
