45ebc9000a1705c17257c447ab39c8d5ef394ea3
Josie Flôres <drijosie@gmail.com>
2022-06-23 12:17:45 -0300

    Event Binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -14,9 +14,17 @@ export class DataBidingComponent implements OnInit {
   public checkedDisabled: boolean = false;
   public imgSrc: string = "https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular-400x250.png";
   public imgTitle: string = "Property Binding";
+  public position: {x: number, y: number} = {x: 0, y:0}
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public alertaInfo(valor: MouseEvent){
+    console.log(valor);
   }
 
+  public mouseMoveTeste(valor: MouseEvent){
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
