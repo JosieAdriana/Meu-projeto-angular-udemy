0ba5170e3d0ccfedb537c39e52af6844c068ee3d
Josie Flôres <drijosie@gmail.com>
2022-06-23 11:01:19 -0300

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,17 @@
-import { Component, OnInit, DoCheck,
-  AfterContentInit,
-  AfterViewInit,
-  AfterViewChecked } from '@angular/core';
-/*
-DoCheck,
-AfterContentInit,
-AfterViewInit,
-AfterViewChecked
-*/
-
+import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-title *ngIf="destruir"></app-title>
-  <br>
-  <button (click)="destruirComponent()">Destruir componente</button>
-  {{ valor }}
-  <button (click)="adicionar()">Adicionar</button>
-  <app-title></app-title>
-  <router-outlet></router-outlet>
+   <app-data-biding></app-data-biding>
+   <router-outlet></router-outlet>
 `
 })
-export class AppComponent implements OnInit, DoCheck,
-AfterContentInit,
-AfterViewInit,
-AfterViewChecked {
-  public valor: number = 1;
+export class AppComponent implements OnInit{
 
-  public destruir:boolean = true;
- constructor() {  
-}
-
-public adicionar(): number {
- return this.valor += 1
-}
- ngOnInit(): void {}
-
- ngDoCheck(): void {
-  console.log("ngDoCheck");
- }
-
- ngAfterContentInit(): void {
-  console.log("ngAfterContentInit");
- }
-
- ngAfterViewChecked(): void {
-  console.log("ngAfterViewChecked");
- }
-
- ngAfterViewInit(): void {
-  console.log("ngAfterViewInit");
- }
- public destruirComponent(){
-  this.destruir = false;
- }
+  constructor() {}
+ 
+  ngOnInit(): void {
+    
+  }
 }
 
